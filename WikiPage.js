dd3e0433a7e2434404274836e2f5902e0e80abff
beppek <beppe@live.se>
2017-11-30 19:36:44 +0100

    set title and content

--- WikiPage.js
+++ WikiPage.js
@@ -1,7 +1,15 @@
 class WikiPage {
   constructor(path) {
-    console.log("Hej");
     this.path = path;
+    this.setTitle(path.substring(6, path.length))
+  }
+
+  setTitle(rawTitle) {
+    this.title = rawTitle.replace("[^a-zA-Z0-9.\\-]", "_");
+  }
+
+  setContent(rawHTML) {
+    this.html = rawHTML;
   }
 }
 
