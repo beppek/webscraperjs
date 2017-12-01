20877c76020e25ff903cc45b54febf8866c23d2a
Beppe Karlsson <beppe@live.se>
2017-12-01 01:01:23 +0100

    prompts user for page input

--- index.js
+++ index.js
@@ -1,10 +1,39 @@
+let prompt = require("prompt");
 let Crawler = require("./Crawler");
 let WikiPage = require("./WikiPage");
 
-let pages = new Set([
-  new WikiPage("/wiki/Ethereum"),
-  new WikiPage("/wiki/Floorball")
-]);
+let regexp = /\/wiki\/(?!.*:)(.*?)/;
 
-let c = new Crawler(pages);
-c.crawl();
+let props = [
+  {
+    name: 'FirstPage',
+    validator: regexp,
+    warning: 'The link should begin with /wiki/ and everything that follows. Anything before that is not needed.'
+  },
+  {
+    name: 'SecondPage',
+    validator: regexp,
+    warning: 'The link should begin with /wiki/ and everything that follows. Anything before that is not needed.'
+  }
+]
+
+let pages = new Set();
+
+prompt.start();
+console.log('Please provide 2 pages from Wikipedia that should be the root for the crawler. Input should begin with /wiki/ and everything that follows. Anything before that is not needed');
+
+prompt.get(props, (err, res) => {
+  if (err) {
+    return console.log(err);
+  }
+  console.log('You have selected the following pages:');
+  console.log(res.FirstPage);
+  console.log(res.SecondPage);
+  for (const page in res) {
+    if (res.hasOwnProperty(page)) {
+      pages.add(new WikiPage(res[page]));
+    }
+  }
+  let c = new Crawler(pages);
+  c.crawl();
+});
